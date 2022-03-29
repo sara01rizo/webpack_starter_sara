cb5ee3ea4f3b821fd64d3e09c025d6baab79df7e
sara-pulido <sara01rizo@gmail.com>
2022-03-29 14:57:20 +0200

    Add: axios pay attention to BundleAnalyzerPlugin

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-import generateTip from "./generateTip";
+import generateJoke from './generateJoke'
 import './styles/main.scss'
-import pluma from './assets/pluma.jpg'
+import laughing from './assets/laughing.svg'
 
-console.log(generateTip())
+const laughImg = document.getElementById('laughImg')
+laughImg.src = laughing
+
+const jokeBtn = document.getElementById('jokeBtn')
+jokeBtn.addEventListener('click', generateJoke)
+
+generateJoke()
